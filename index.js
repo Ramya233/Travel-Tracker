b2a20c6a4f49ef7b2e034a32fbf7660f81aa26d2
Ramya <ramyapoojari31@gmail.com>
2024-04-17 14:26:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,94 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pkg from "pg";
+import ejs from "ejs";
+import { fileURLToPath } from "url";
+import path from "path";
+import { dirname, join } from "path";
+import cors from "cors";
+import env from "dotenv";
+import { sql } from "@vercel/postgres";
+
+const { Pool } = pkg;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+env.config();
+
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+});
+
+pool.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Error connecting to the database', err));
+
+app.use(cors());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static("public"));
+app.use(express.static(__dirname + "/public/"));
+
+async function checkVisisted() {
+  const result = await db.query("SELECT country_code FROM visited_countries");
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  return countries;
+}
+// GET home page
+app.get("/", async (req, res) => {
+  const countries = await checkVisisted();
+  res.render("index.ejs", { countries: countries, total: countries.length });
+});
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  try {
+    const result = await db.query(
+      "SELECT country_code FROM countries WHERE LOWER(country_name) = $1;",
+      [input.toLowerCase()]
+    );
+    // "SELECT country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
+
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+    try {
+      await db.query(
+        "INSERT INTO visited_countries (country_code) VALUES ($1)",
+        [countryCode]
+      );
+      res.redirect("/");
+    } catch (err) {
+      //you selected the country and while inserting you realized that the country has already been added, tell the user already added
+      console.log(err);
+      const countries = await checkVisisted();
+      res.render("index.ejs", {
+        countries: countries,
+        total: countries.length,
+        error: "Country has already been added, try again.",
+      });
+    }
+  } catch (err) {
+    //countries does not exists, give the existing countries and error.
+    console.log(err);
+    const countries = await checkVisisted();
+    res.render("index.ejs", {
+      countries: countries,
+      total: countries.length,
+      error: "Country name does not exist, try again.",
+    });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
